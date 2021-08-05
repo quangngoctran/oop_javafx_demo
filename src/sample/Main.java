909b99d63e9518bc382154ce359425050d2606c8
quangngoctran <quang.tran@rmit.edu.vn>
2021-08-05 22:48:42 +0700

    Changes the flags and updates gridPane.add(iv, i % 2 + 1, i / 2 + 1)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,29 +10,30 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    GridPane createGridPane() {
-        String[] urls = {"https://cdn.pixabay.com/photo/2012/04/10/23/04/vietnam-26834_1280.png",
-                "https://www.nicepng.com/png/full/780-7804075_canada-flag-png-transparent-icon-canada-flag-vector.png",
-                "https://png.pngtree.com/png-clipart/20200226/original/pngtree-australia-flag-transparent-with-fabric-png-image_5312022.jpg",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Flag_of_Japan_%28bordered%29.svg/640px-Flag_of_Japan_%28bordered%29.svg.png"
+    private GridPane createGridPane() {
+        String[] urls = {
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1920px-Flag_of_Vietnam.svg.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/1200px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png",
+                "https://images-na.ssl-images-amazon.com/images/I/51YCk1KrXWL._SL1414_.jpg",
+                "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png"
         };
 
         GridPane gridPane = new GridPane();
         ImageView iv;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < urls.length; i++) {
             iv = new ImageView(urls[i]);
-            iv.setFitWidth(400);
+            iv.setFitWidth(300);
             iv.setPreserveRatio(true);
             iv.setSmooth(true);
-            gridPane.add(iv, (i < 2)? 1 : 2, i % 2 + 1);
+            gridPane.add(iv, i % 2 + 1, i / 2 + 1);
         }
 
         return gridPane;
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         Scene scene = new Scene(createGridPane());
         primaryStage.setScene(scene);
